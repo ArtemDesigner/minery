5b7a7849bd30cd06dfadf84e268de8d652a4cb41
Yevhenii Rizchenko <veuzqpvo@email.com>
2021-03-24 11:06:20 +0200

    markup

--- src/js/main.js
+++ src/js/main.js
@@ -27,23 +27,23 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
-$(function () {
-  (function quantityProducts() {
-    var $quantityArrowMinus = $(".quantity-arrow-minus");
-    var $quantityArrowPlus = $(".quantity-arrow-plus");
-    var $quantityNum = $(".quantity-num");
+// $(function () {
+//   (function quantityProducts() {
+//     var $quantityArrowMinus = $(".quantity-arrow-minus");
+//     var $quantityArrowPlus = $(".quantity-arrow-plus");
+//     var $quantityNum = $(".quantity-num");
 
-    $quantityArrowMinus.click(quantityMinus);
-    $quantityArrowPlus.click(quantityPlus);
+//     $quantityArrowMinus.click(quantityMinus);
+//     $quantityArrowPlus.click(quantityPlus);
 
-    function quantityMinus() {
-      if ($quantityNum.val() > 1) {
-        $quantityNum.val(+$quantityNum.val() - 1);
-      }
-    }
+//     function quantityMinus() {
+//       if ($quantityNum.val() > 1) {
+//         $quantityNum.val(+$quantityNum.val() - 1);
+//       }
+//     }
 
-    function quantityPlus() {
-      $quantityNum.val(+$quantityNum.val() + 1);
-    }
-  })();
-});
+//     function quantityPlus() {
+//       $quantityNum.val(+$quantityNum.val() + 1);
+//     }
+//   })();
+// });
